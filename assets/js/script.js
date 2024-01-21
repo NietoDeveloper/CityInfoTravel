a97f7770e04da2efcf729163d12cdddcafc1b063
Manuel <NietoSoftwareDeveloper@outlook.com>
2024-01-20 22:11:16 -0500

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,17 @@
 import { barcelona, roma, paris, londres } from './ciudades.js'
 
-// Obtener los elementos del DOM
 let enlaces = document.querySelectorAll('a')
 let tituloElemento = document.getElementById('titulo')
 let subTituloElemento = document.getElementById('subtitulo')
 let parrafoElemento = document.getElementById('parrafo')
 let precioElemento = document.getElementById('precio')
 
-//Agregar un evento CLICK a cada enlace
 enlaces.forEach(function (enlace) {
     enlace.addEventListener('click', function () {
-        // Remover la clase "active" de todos los enlaces
         enlaces.forEach(function (enlace) {
             enlace.classList.remove('active');
         });
-        
-        // Agregar la clase "active" al enlace actual
         this.classList.add('active')
-
-        // Obtener el contenido correspondiente según el enlace
         let contenido = obtenerContenido(this.textContent)
 
         tituloElemento.innerHTML = contenido.titulo
@@ -28,7 +21,6 @@ enlaces.forEach(function (enlace) {
     });
 });
 
-//Función para traer la información correcta desde ciudades.js
 function obtenerContenido(enlace) {
     let contenido = {
         'Barcelona': barcelona,
